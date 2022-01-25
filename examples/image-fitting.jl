79795bf94cf550d6565bd801e228a2c61e8a9d6b
William Thompson <will.thompson@outlook.com>
2022-01-25 07:10:54 -0800

    Update image examples

--- examples/image-fitting.jl
+++ examples/image-fitting.jl
@@ -13,157 +13,260 @@ using Plots
 # Generate astrometry points using this template
 # and we will try to fit the results
 truths = [
-    # KeplerianElements(;a = 12,τ = 0.25,ω = 0,Ω = 0,e = 0.1,i = 0.5,μ = 1.,plx = 45.,),
-    KeplerianElements(;a = 12,τ = 0.65,ω = 0,Ω = 0,e = 0.1,i = 0.5,μ = 1.,plx = 45.,),
-    # KeplerianElements(;a = 18, τ = 0.75,ω = 0,Ω = 0,e = 0.15, i = 0.5,μ = 1.,plx = 45.,),
+    KeplerianElements(; a = 12, τ = 0.15, ω = 0.5, Ω = 0, e = 0.2, i = 0.65pi, μ = 1.0, plx = 45.0),
+
+    # KeplerianElements(;a = 12,τ = 0.65,ω = 0,Ω = 0,e = 0.1, i = deg2rad(90), μ = 1.,plx = 45.,),
+
 ]
 intensities = [
     1800,
-    # 3000,
-    # 650,
-    # 1000,
+    # 3500,
 ]
 
 # truth_elements = KeplerianElements(ComponentArray(truth, static))
 
 # times = range(0, period(truth_elements)*4/5, length=9, )
-times = range(0, period(first(truths))/2, length=10,)
+# times = range(0, 0.6period(first(truths)), length = 2,)
+times = range(0, 0.3period(first(truths)), length = 2,)
 
 
 # Create synthetic images at each time with those points
 Random.seed!(1234)
 images_contrasts = map(times) do t
-    img = 20000fakeimage((201,201),1.2)
+    img = 20000fakeimage((201, 201), 1.2)
     img = centered(img)
+    img_copy = copy(img)
     for (inten, truth) in zip(intensities, truths)
         ra, dec = kep2cart(truth, t)
         x = -ra
         y = dec
-        img[round(Int,x/10), round(Int,y/10)] += inten #2500 #+ 300randn()
+        img[round(Int, x / 10), round(Int, y / 10)] += inten #2500 #+ 300randn()
     end
 
-    img = imfilter(img, Kernel.gaussian(5), "replicate")
-    contrast = contrast_interp(img)
 
+    img = imfilter(img, Kernel.gaussian(5), "replicate")
+    img_copy = imfilter(img_copy, Kernel.gaussian(5), "replicate")
+
+    sep = centered(DirectImages.imgsep(img))
+    img[sep.<12] .= NaN
+    img_copy[sep.<12] .= NaN
+
+    contrast = contrast_interp(img_copy)
     img, contrast
 end
-images = [img for (img,contrast) in images_contrasts]
-contrasts = [contrast for (img,contrast) in images_contrasts]
+images = [img for (img, contrast) in images_contrasts]
+contrasts = [contrast for (img, contrast) in images_contrasts]
 nothing
 
 ##
-# imshow2(reduce(hcat, images), clims=(-35,35))
+imshow2(reduce(vcat, images), clims = (-35, 35))
 
 ##
 
-planet_priors = Priors(
-    a = Uniform(7, 16),
-    e = TruncatedNormal(0.1, 0.1, 0.0, 0.4),
-    τ = Uniform(0,1),
-    # τ = Uniform(0.4,0.8),
-    ω = Uniform(-π,π),
-    # ω = Uniform(-π/4,π/4),
-    i = Uniform(-π,π),
-    # i = Uniform(-π/4,π/4),
-    # i = TruncatedNormal(0, 0.2, -π,π),
-    Ω = Uniform(-π,π),
-    # Ω = Uniform(-π/4,π/4),
+@named b = Planet(
+    Deterministic(
+        J = (sys, pl) -> 10^pl.logJ,
+    ),
+    Priors(
+        # a = Uniform(7, 16),
+        # e = TruncatedNormal(0.1, 0.1, 0.0, 0.4),
+        # τ = Uniform(0,1),
+        # # τ = Uniform(0.4,0.8),
+        # ω = Uniform(-π,π),
+        # # ω = Uniform(-π/4,π/4),
+        # i = Uniform(-π,π),
+        # # i = Uniform(-π/4,π/4),
+        # # i = TruncatedNormal(0, 0.2, -π,π),
+        # Ω = Uniform(-π,π),
+        # # Ω = Uniform(-π/4,π/4),
 
-    # mass = Uniform(0mjup2msol,70mjup2msol),
+        # # mass = Uniform(0mjup2msol,70mjup2msol),
 
-    J = Uniform(0,20)
+        a = Normal(12, 1),
+        e = Beta(10, 70),
+        τ = Normal(0.5, 1),
+        ω = Normal(1pi, 1pi),
+        i = Normal(0.6pi, 0.3pi),
+        Ω = Normal(0.5pi, 0.5pi),
+        logJ = Normal(1, 0.5)
+        # J = Normal(0, 20)
+    )
 )
-planet = reparameterize(DirectDetections.Planet(planet_priors))
-nothing
-##
 
 
 system_images = DirectDetections.Images(
-    (band=:J, image=images[1], platescale=10.0, epoch=times[1]),#, contrast=contrasts[1]),
-    (band=:J, image=images[2], platescale=10.0, epoch=times[2]),#, contrast=contrasts[2]),
-    (band=:J, image=images[3], platescale=10.0, epoch=times[3]),#, contrast=contrasts[3]),
-    (band=:J, image=images[4], platescale=10.0, epoch=times[4]),#, contrast=contrasts[4]),
+    [
+        (band = :J, image = images[i], platescale = 10.0, epoch = times[i], contrast = contrasts[i])
+        for i in eachindex(images)
+    ]...
+    # (band=:J, image=images[1], platescale=10.0, epoch=times[1]),#, contrast=contrasts[1]),
+    # (band=:J, image=images[2], platescale=10.0, epoch=times[2]),#, contrast=contrasts[2]),
+    # (band=:J, image=images[3], platescale=10.0, epoch=times[3]),#, contrast=contrasts[3]),
+    # (band=:J, image=images[4], platescale=10.0, epoch=times[4]),#, contrast=contrasts[4]),
 )
 
 
 system_priors = Priors(
-    μ = TruncatedNormal(1.0, 0.01, 0, 10),
-    plx = TruncatedNormal(45., 0.0001, 0, 100),
+    μ = Normal(1.0, 0.01),
+    plx = Normal(45.0, 0.0001),
 )
 
-# system = System(system_priors, system_pma, system_images, planet_b)
-system = System(system_priors, system_images, planet,)
-nothing
+
+@named system = System(system_priors, system_images, b)
+# @named system = System(system_priors, b)
 
 ##
-@time chains_img = DirectDetections.mcmc(
-    system;
-    # numwalkers=800,
-    # burnin=62_000,
-    # numsamples_perwalker=100_000,
-    # thinning=250,
-
-    numwalkers=50,
-    burnin=1000,
-    numsamples_perwalker=5_000,
-    thinning=1,
-    
-    
-    squash = false
+out = DirectDetections.hmc(
+    system, 0.65;
+    # adaptation = 5_000,
+    adaptation = 200,
+    iterations = 5_000,
+    initial_samples=5_000,
+    # step_size=7e-4,
 );
-nothing
 
 ##
-@time chainsh, stats = DirectDetections.hmc(
-    system;
-    burnin=10_000,
-    numwalkers=10,
-    numsamples_perwalker=15_000,
-);
-nothing
+# out = DirectDetections.temperedhmc(
+#     system, 0.65;
+#     # adaptation = 5_000,
+#     adaptation = 500,
+#     iterations = 5_000,
+#     initial_samples=5_000,
+#     # step_size=1e-3,
+# );
+# nothing
 
+# Big differences between:
+# GeneralisedNoUTurn
+# SliceTS
+# MultinomialTS
+# StrictGeneralisedNoUTurn
+##
+(;chain, stats, adaptor) = out
+plotmodel(chain, system)#, lims=1000)
+
+##
+ra, dec = projectpositions(chain, :b, last(times))
+i = DirectImage(images[end])
+i.platescale = 10
+imshow(i, skyconvention=true)
+ii = rand(eachindex(ra), 500)
+scatter!(ra[ii], dec[ii], markerstrokewidth=0, color=:blue, ms=3, alpha=0.5)
+##
+function snr(phot)
+    low, med, up = quantile(reshape(phot, :), (0.33, 0.5, 0.67))
+    st = std(reshape(phot, :))
+    return med / (up - low)
+end
+snr(chain["b[J]"])
+
+##
+
+
+
+
+##
+ra, dec = projectpositions(chain, :b, first(times))
+pa1 = atan.(dec, ra)
+sep1 = sqrt.(ra .^ 2 .+ dec .^ 2)
+##
+ra, dec = projectpositions(chain, :b, last(times))
+pa2 = atan.(dec, ra)
+sep2 = sqrt.(ra .^ 2 .+ dec .^ 2)
+
+##
+table = (;
+    # pa1 = rad2deg.(pa1),
+    # sep1,
+    # pa2 = rad2deg.(pa2),
+    # sep2,
+    a = vec(chain["b[a]"]),
+    J = vec(chain["b[J]"]),
+    e= vec(chain["b[e]"]),
+    i=rad2deg.(rem.(vec(chain["b[i]"]), 2pi, RoundDown)),
+    Ω=rad2deg.(rem.(vec(chain["b[Ω]"]), 1pi, RoundDown)),
+    ω=rad2deg.(rem.(vec(chain["b[ω]"]), 2pi, RoundDown)),
+    τ=         rem.(vec(chain["b[τ]"]),1,RoundDown),
+)
+labels = [
+    # "\\theta_1",
+    # "r_1",
+    # "\\theta_2",
+    # "r_2",
+    "a",
+    "J",
+    "e",
+    "i",
+    "\\Omega",
+    "\\omega",
+    "\\tau",
+]
+units = [
+    # "(\\degree)",
+    # "(mas)",
+    # "(\\degree)",
+    # "(mas)",
+    "(au)",
+    "(arb.)",
+    "",
+    "(\\degree)",
+    "(\\degree)",
+    "(\\degree)",
+    "",
+]
+# labels, units, 
+using PairPlots
+# PairPlots.corner(table, plotscatter=false, hist2d_kwargs=(;color=:white, nbins=15), hist_kwargs=(;nbins=15,framestyle=:none))
+PairPlots.corner(table, labels, units)
+
+##
+using StatsBase
+h = fit(Histogram, (dec,ra), (-1000:100:1000,-1000:100:1000))
+contour(h.weights)#, levels=[quantile(h.weights[:], 0.39)])
+##
 ##
 [count(getproperty.(s, :numerical_error)) for s in stats]
+[mean(getproperty.(s, :acceptance_rate)) for s in stats]
 ##
 function orbsonimg(args...; kwargs...)
     plot()
-    orbsonimg!(args...;kwargs...)
+    orbsonimg!(args...; kwargs...)
 end
-function orbsonimg!(planet, image, N=100; kwargs...)
-    sampled = sampleorbits(planet, N);
+function orbsonimg!(planet, image, N = 100; kwargs...)
+    sampled = sampleorbits(planet, N)
     i = DirectImage(image)
-    i.PLATESCALE = 10.
-    imshow!(i;skyconvention=true,kwargs...)
+    i.PLATESCALE = 10.0
+    imshow!(i; skyconvention = true, kwargs...)
     # plot!(sampled,color=:white,alpha=5/N,label="")
-    plot!(sampled,color=:white,label="",)
-    xlims!(i.PLATESCALE.*extrema(axes(i,1)))
-    ylims!(i.PLATESCALE.*extrema(axes(i,2)))
+    plot!(sampled, color = :white, label = "",)
+    xlims!(i.PLATESCALE .* extrema(axes(i, 1)))
+    ylims!(i.PLATESCALE .* extrema(axes(i, 2)))
 
 end
 
 
 ##
-imshow(images[3],clims=(-2,10))
+imshow(images[3], clims = (-2, 10))
 # orbsonimg(chains.planets[1],images[2],clims=(-2,10))
-orbsonimg(chains.planets[1],mean(images), 500,legend=nothing, clims=(-0.2,1),color=:magma,dpi=200)
-plot!(truths, color=:black, lw=2, ls=:dash, label="")
+orbsonimg(chains.planets[1], mean(images), 500, legend = nothing, clims = (-0.2, 1), color = :magma, dpi = 200)
+plot!(truths, color = :black, lw = 2, ls = :dash, label = "")
 # orbsonimg(chainsh[2].planets[1],images)
 # writefits("tmp.fits", images[2])
 ##
 plot()
-orbsonimg!(chainsh[1].planets[1],mean(images),1,legend=nothing, clims=(-0.2,1),color=:magma,dpi=200,)
+orbsonimg!(chainsh[1].planets[1], mean(images), 1, legend = nothing, clims = (-0.2, 1), color = :magma, dpi = 200,)
 for c in chainsh
-    plot!(sampleorbits(c.planets[1],125),color=:white,label="",alpha=0.1)
+    plot!(sampleorbits(c.planets[1], 125), color = :white, label = "", alpha = 0.1)
 end
-    plot!(truths, color=:black, lw=2, ls=:dash, label="",colorbar=nothing)
+plot!(truths, color = :black, lw = 2, ls = :dash, label = "", colorbar = nothing)
 
 ##
 
 
 function snr(phot)
-    low,med,up = quantile(reshape(phot,:),(0.33, 0.5, 0.67))
-    st = std(reshape(phot,:))
-    return med/(up-low)
+    low, med, up = quantile(reshape(phot, :), (0.33, 0.5, 0.67))
+    st = std(reshape(phot, :))
+    return med / (up - low)
 end
 
 ##
@@ -177,34 +280,34 @@ histogram(chains.planets[1].phot.Keck_L′[:])
 histogram(chainsh[1].planets[1].phot.Keck_L′[:])
 ##
 DirectDetections.plotposterior(chains_img, 1, :a, 500)
-plot!(truths, color=:black, lw=2, ls=:dash, label="", alpha=0.5)
+plot!(truths, color = :black, lw = 2, ls = :dash, label = "", alpha = 0.5)
 # savefig("images/readme-orbits.png")
 
 ##
-DirectDetections.plotposterior(chains_img, chains_img.planets[1], :i, 500, colorbartitle="inclination (rad)",dpi=200)
+DirectDetections.plotposterior(chains_img, chains_img.planets[1], :i, 500, colorbartitle = "inclination (rad)", dpi = 200)
 # savefig("images/readme-post-i.png")
 
 ##
-DirectDetections.plotposterior(chains_img, chains_img.planets[1], (:phot, :Keck_L′), 500, colorbartitle="flux", cmap=:plasma, rev=false, clims=(0,12),dpi=200)
-plot!(truths, color=:black, lw=2, ls=:dash, label="")
+DirectDetections.plotposterior(chains_img, chains_img.planets[1], (:phot, :Keck_L′), 500, colorbartitle = "flux", cmap = :plasma, rev = false, clims = (0, 12), dpi = 200)
+plot!(truths, color = :black, lw = 2, ls = :dash, label = "")
 # savefig("images/readme-post-f.png")
 
 ##
-DirectDetections.plotposterior(chains.planets[1], :e, 500, colorbartitle="eccentricity",dpi=200)
-plot!(truths, color=:black, lw=2, ls=:dash)
+DirectDetections.plotposterior(chains.planets[1], :e, 500, colorbartitle = "eccentricity", dpi = 200)
+plot!(truths, color = :black, lw = 2, ls = :dash)
 # savefig("images/readme-post-e.png")
 
 ##
-DirectDetections.plotposterior(chains.planets[1],  lw=3, :e, 500, colorbartitle="eccentricity",dpi=200, cmap=:plasma, rev=false,)
-plot!(truths, color=:black, lw=2, ls=:dash)
+DirectDetections.plotposterior(chains.planets[1], lw = 3, :e, 500, colorbartitle = "eccentricity", dpi = 200, cmap = :plasma, rev = false,)
+plot!(truths, color = :black, lw = 2, ls = :dash)
 # savefig("images/readme-post-e2.png")
 
 ##
 
 ra, dec = projectpositions(chains.planets[1], mean(times))
-histogram2d(ra,dec,aspectratio=1, color=:plasma, background_inside=:black, framestyle=:box,xflip=true,dpi=200, colorbartitle=".\n\nposterior density")
-xlims!(1.25.*(-1,+1).*max(abs.(extrema(ra))...))
-ylims!(1.25.*(-1,+1).*max(abs.(extrema(dec))...))
+histogram2d(ra, dec, aspectratio = 1, color = :plasma, background_inside = :black, framestyle = :box, xflip = true, dpi = 200, colorbartitle = ".\n\nposterior density")
+xlims!(1.25 .* (-1, +1) .* max(abs.(extrema(ra))...))
+ylims!(1.25 .* (-1, +1) .* max(abs.(extrema(dec))...))
 xlabel!("Δ right ascension (as)")
 ylabel!("Δ declination (as)")
 
@@ -212,14 +315,14 @@ ylabel!("Δ declination (as)")
 mask = (-200 .< ra .< 250) .& (300 .< dec .< 800)
 snr(chains.planets[1].phot.Keck_L′)
 snr(chains.planets[1].phot.Keck_L′[mask])
-histogram2d(ra[mask],dec[mask],aspectratio=1, color=:plasma, background_inside=:black, framestyle=:box, cscale=:log10kep2cart,dpi=200)
-histogram2d(ra[mask],dec[mask],aspectratio=1, color=:plasma, background_inside=:black, framestyle=:box, cscale=:log10kep2cart,dpi=200)
+histogram2d(ra[mask], dec[mask], aspectratio = 1, color = :plasma, background_inside = :black, framestyle = :box, cscale = :log10kep2cart, dpi = 200)
+histogram2d(ra[mask], dec[mask], aspectratio = 1, color = :plasma, background_inside = :black, framestyle = :box, cscale = :log10kep2cart, dpi = 200)
 # xlims!(1.25.*(-1,+1).*max(abs.(extrema(ra))...))
 # ylims!(1.25.*(-1,+1).*max(abs.(extrema(dec))...))
 ##
-ras,decs = projectpositions(chains.planets[1],mean(times))
+ras, decs = projectpositions(chains.planets[1], mean(times))
 # histogram2d(ras[5end÷6:end], decs[5end÷6:end])
-histogram2d(ras, decs,aspectratio=1,xflip=true,color=:plasma)
+histogram2d(ras, decs, aspectratio = 1, xflip = true, color = :plasma)
 
 ##
 mask = 7 .< chains.planets[1].a .< 15
@@ -231,87 +334,87 @@ snr(chains.planets[1].phot.Keck_L′[mask])
 
 ##
 table = (;
-    L=chains.planets[1].phot.Keck_L′,
+    L = chains.planets[1].phot.Keck_L′,
     chains.planets[1].a,
     chains.planets[1].i,
     chains.planets[1].e,
-    tau=chains.planets[1].τ,
+    tau = chains.planets[1].τ
 )
 corner(
     table,
     split(raw"\mathrm{flux} a i e \tau"),
-    plotscatter=false,
-    hist2d_kwargs=(;ylims=(0,NaN),xlims=(0,NaN)),
-    scatter_kwargs=(;markersize=0.5),
-    dpi=200,
+    plotscatter = false,
+    hist2d_kwargs = (; ylims = (0, NaN), xlims = (0, NaN)),
+    scatter_kwargs = (; markersize = 0.5),
+    dpi = 200,
     # lim_factor=2,
     # hist2d_kwargs=(;nbins=8),
 )
 
 ##
 ra, dec = projectpositions(chains.planets[1], mean(times))
-pa = atan.(dec,ra)
+pa = atan.(dec, ra)
 # mask = (pa .< 0.5) .& 
 #     (600 .< ra .< 800) .&
 #     (-200 .< dec .< -100)
 mask = vec((chains.planets[1].phot.Keck_L′ .< 6))
 table = (;
-    ra=ra[mask],
-    dec=dec[mask],
+    ra = ra[mask],
+    dec = dec[mask],
     # pa=rad2deg.(pa[mask]),
-    a=(chains.planets[1].a)[mask],
-    L=(chains.planets[1].phot.Keck_L′)[mask],
-    i=rad2deg.((chains.planets[1].i))[mask],
-    e=(chains.planets[1].e)[mask],
+    a = (chains.planets[1].a)[mask],
+    L = (chains.planets[1].phot.Keck_L′)[mask],
+    i = rad2deg.((chains.planets[1].i))[mask],
+    e = (chains.planets[1].e)[mask]
 )
 corner(
     table,
     # split(raw"a \mathrm{flux} i e \tau"),
-    plotscatter=false,
+    plotscatter = false,
     # hist2d_kwargs=(;ylims=(0,NaN),xlims=(0,NaN)),
     # scatter_kwargs=(;markersize=0.5),
     # dpi=200,
     # lim_factor=2,
-    hist_kwargs=(;nbins=50),
-    hist2d_kwargs=(;nbins=50),
+    hist_kwargs = (; nbins = 50),
+    hist2d_kwargs = (; nbins = 50),
 )
 
 
 
 ## Chains
 table = (;
-    a=(chains.planets[1].a),
-    L=(chains.planets[1].phot.Keck_L′),
-    i=rad2deg.((chains.planets[1].i)),
-    e=(chains.planets[1].e),
+    a = (chains.planets[1].a),
+    L = (chains.planets[1].phot.Keck_L′),
+    i = rad2deg.((chains.planets[1].i)),
+    e = (chains.planets[1].e)
 )
 corner(
     table,
     # split(raw"a \mathrm{flux} i e \tau"),
-    plotscatter=false,
+    plotscatter = false,
     # hist2d_kwargs=(;ylims=(0,NaN),xlims=(0,NaN)),
     # scatter_kwargs=(;markersize=0.5),
     # dpi=200,
     # lim_factor=2,
-    hist_kwargs=(;nbins=50),
-    hist2d_kwargs=(;nbins=50),
+    hist_kwargs = (; nbins = 50),
+    hist2d_kwargs = (; nbins = 50),
 )
 
 ## Chainsh
 table = (;
-    a=reduce(vcat, [c.planets[1].a for c in chainsh]),
-    L=reduce(vcat, [c.planets[1].phot.Keck_L′ for c in chainsh]),
-    i=rad2deg.(reduce(vcat, [c.planets[1].i for c in chainsh])),
-    e=reduce(vcat, [c.planets[1].e for c in chainsh]),
+    a = reduce(vcat, [c.planets[1].a for c in chainsh]),
+    L = reduce(vcat, [c.planets[1].phot.Keck_L′ for c in chainsh]),
+    i = rad2deg.(reduce(vcat, [c.planets[1].i for c in chainsh])),
+    e = reduce(vcat, [c.planets[1].e for c in chainsh])
 )
 corner(
     table,
     # split(raw"a \mathrm{flux} i e \tau"),
-    plotscatter=false,
+    plotscatter = false,
     # hist2d_kwargs=(;ylims=(0,NaN),xlims=(0,NaN)),
     # scatter_kwargs=(;markersize=0.5),
     # dpi=200,
     # lim_factor=2,
-    hist_kwargs=(;nbins=25),
-    hist2d_kwargs=(;nbins=25),
+    hist_kwargs = (; nbins = 25),
+    hist2d_kwargs = (; nbins = 25),
 )
--- examples/images-example.jl
+++ examples/images-example.jl
@@ -1,16 +1,5 @@
 using DirectDetections, Distributions, Plots
 
-@named X = DirectDetections.Planet(
-    Priors(
-        a = Normal(16, 3),
-        e = TruncatedNormal(0.2, 0.2, 0, 1.0),
-        τ = Normal(0.5, 1),
-        ω = Normal(0.1, deg2rad(30.)),
-        i = Normal(0.55, deg2rad(10.)),
-        Ω = Normal(0.0, deg2rad(30.)),
-        GPI_H = Normal(5.5, 1)
-    ),
-)
 
 
 
@@ -29,13 +18,18 @@ imshow2([
 ##
 
 
-system_images = DirectDetections.Images(
-    (band=:GPI_H, image=images[1], platescale=10.0, epoch=1238.6),
-    (band=:GPI_H, image=images[2], platescale=10.0, epoch=1584.7),
-    (band=:GPI_H, image=images[3], platescale=10.0, epoch=3220.0),
-    (band=:GPI_H, image=images[4], platescale=10.0, epoch=7495.9),
-    (band=:GPI_H, image=images[5], platescale=10.0, epoch=7610.4),
+@named b = DirectDetections.Planet(
+    Priors(
+        a = Normal(16, 3),
+        e = Beta(5, 25),
+        τ = Normal(0.5, 1),
+        ω = Normal(0.1, deg2rad(30.)),
+        i = Normal(0.55, deg2rad(10.)),
+        Ω = Normal(0.0, deg2rad(30.)),
+        H = Normal(5.5, 1)
+    ),
 )
+
 ##
 
 @named HD82134 = System(
@@ -43,15 +37,21 @@ system_images = DirectDetections.Images(
         μ = Normal(2.0, 0.1),
         plx =Normal(45., 0.02),
     ),
-    system_images,
-    X,
+    Images(
+        (band=:H, image=images[1], platescale=10.0, epoch=1238.6),
+        (band=:H, image=images[2], platescale=10.0, epoch=1584.7),
+        (band=:H, image=images[3], platescale=10.0, epoch=3220.0),
+        (band=:H, image=images[4], platescale=10.0, epoch=7495.9),
+        (band=:H, image=images[5], platescale=10.0, epoch=7610.4),
+    ),
+    b,
 )
 
-chain, stats = DirectDetections.hmc(
+chain, stats, extra = DirectDetections.hmc(
     HD82134,
-    adaptation =   8_000,
-    iterations = 100_000,
-    tree_depth =     12,
+    adaptation =  4_000,
+    iterations =  6_000,
+    tree_depth =     10,
 );
 
 ##
@@ -61,13 +61,13 @@ savefig("../docs/src/assets/images-model-plot.svg")
 
 ##using PairPlots
 table = (;
-a=         chain["X[a]"],
-H=         chain["X[GPI_H]"],
-e=         chain["X[e]"],
-i=rad2deg.(chain["X[i]"]),
-Ω=rad2deg.(chain["X[Ω]"]),
-ω=rad2deg.(chain["X[ω]"]),
-τ=         chain["X[τ]"],
+a=         chain["b[a]"],
+H=         chain["b[H]"],
+e=         chain["b[e]"],
+i=rad2deg.(chain["b[i]"]),
+Ω=rad2deg.(chain["b[Ω]"]),
+ω=rad2deg.(chain["b[ω]"]),
+τ=         chain["b[τ]"],
 )
 labels=[
 "a",
@@ -95,6 +95,7 @@ savefig("../docs/src/assets/images-corner-plot.svg")
 
 ## This block generates the images we used for the example
 using Random
+using DirectOrbits
 Random.seed!(1234)
 
 truth = (;
@@ -135,7 +136,7 @@ images_contrasts = map(eachrow(points)) do (ra,dec)
     img_for_contrast = imfilter(img, Kernel.gaussian(5), "replicate")
     contrast = contrast_interp(img_for_contrast)
 
-    img[round(Int,x/10), round(Int,y/10)] += 800
+    img[round(Int,x/10), round(Int,y/10)] += 300#800
 
     img = imfilter(img, Kernel.gaussian(5), "replicate")
 
