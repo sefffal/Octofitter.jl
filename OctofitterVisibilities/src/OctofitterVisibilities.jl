43f330473f576f5cd2109e0328835565926a0c26
William Thompson <wthompson@uvic.ca>
2023-03-08 21:24:46 +0000

    Skeleton of visibility support

---
+++ OctofitterVisibilities/src/OctofitterVisibilities.jl
@@ -0,0 +1,97 @@
+module OctofitterVisibilities
+
+using Octofitter
+using PlanetOrbits
+using Tables, TypedTables
+
+const vis_cols = (:epoch, :col1, :col2)
+struct VisibiltiesLikelihood{TTable<:Table} <: Octofitter.AbstractLikelihood
+    table::TTable
+    function VisibiltiesLikelihood(observations...)
+        table = Table(observations...)
+        if !issubset(vis_cols, Tables.columnnames(table))
+            error("Ecpected columns $vis_cols")
+        end
+        return new{typeof(table)}(table)
+    end
+end
+VisibiltiesLikelihood(observations::NamedTuple...) = VisibiltiesLikelihood(observations)
+export VisibiltiesLikelihood
+
+"""
+Visibliitiy modelling likelihood for point sources.
+"""
+function Octofitter.ln_like(vis::VisibiltiesLikelihood, θ_system, orbits, num_epochs::Val{L}=Val(length(rv.table))) where L
+    T = typeof(θ_system.M)
+    ll = zero(T)
+
+    # Access the data here: 
+    epochs = vis.table.epoch
+    bands = vis.table.band
+    # and observations too
+
+    # `θ_system` contains all parameter values, e.g. photometry of each planet
+    
+    # `orbits` creates pre-constructor orbit objects (one per planet) though these could be created from `θ_system`
+    # We can use `orbits` to calculate the position of a planet on a given epoch.
+
+    for i in eachindex(epochs)
+        epoch = epochs[i]
+        band = bands[i]
+
+        # Loop through planets
+        for i in eachindex(orbits)
+            # All parameters relevant to this planet
+            θ_planet = θ_system.planets[i]
+            # orbit object pre-created from above parameters (shared between all likelihood functions)
+            orbit = orbits[i]
+
+            sol = orbitsolve(orbit, epoch)
+            Δra  = raoff(sol)  # in mas
+            Δdec = decoff(sol) # in mas
+
+            # Get model flux parameter in this band (band provided as a symbol, e.g. :L along with data in table row.)
+            f_band = getproperty(θ_planet, band)
+    
+
+            # Accumulate into likelihood
+            # ll += ...
+        end
+    end
+
+
+    return ll
+end
+
+
+
+
+# # Generate new observations for a planet (I don't think this is relevant)
+# function Octofitter.genobs(like::VisibiltiesLikelihood, elem::VisualOrbit, θ_planet)
+
+# end
+
+
+
+
+# Generate new observations for a system of possibly multiple planets
+function Octofitter.genobs(like::VisibiltiesLikelihood, elems::Vector{<:VisualOrbit}, θ_system)
+
+    # # Get epochs, uncertainties, and planet masses from observations and parameters
+    # epochs = like.table.epoch 
+    # σ_rvs = like.table.σ_rv 
+    # planet_masses = [θ_planet.mass for θ_planet in θ_system.planets] .* 0.000954588 # Mjup -> Msun
+
+    # # Generate new star radial velocity data
+    # rvs = radvel.(reshape(elems, :, 1), epochs, transpose(planet_masses))
+    # noise = randn(length(epochs)) .* θ_system.jitter
+    # rvs = sum(rvs, dims=2)[:,1] .+ θ_system.rv .+ noise
+    # radvel_table = Table(epoch=epochs, rv=rvs, σ_rv=σ_rvs)
+
+    # return VisibiltiesLikelihood(radvel_table)
+end
+
+
+
+
+end
