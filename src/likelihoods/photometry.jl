1a000ebd0e909ce3fd46dc3a710dd54394bbb263
William Thompson <will.thompson@outlook.com>
2023-10-05 16:06:49 +0000

    Add missing docstrings

--- src/likelihoods/photometry.jl
+++ src/likelihoods/photometry.jl
@@ -1,6 +1,19 @@
 
 
 const phot_cols = (:band, :phot, :σ_phot)
+
+"""
+    PhotometryLikelihood(
+        (band = :Z, phot=15.0, σ_phot=3.),
+        (band = :J, phot=13.5, σ_phot=0.5),
+        (band = :L, phot=11.0, σ_phot=1.0)
+    )
+
+A likelihood for comparing measured photometry points in one or more 
+filter bands to data (provided here). Requires the `:band`, `:phot',
+and `:σ_phot` columns. Can be provided with any Tables.jl compatible
+data source.
+"""
 struct PhotometryLikelihood{TTable<:Table} <: AbstractLikelihood
     table::TTable
     function PhotometryLikelihood(observations...)
