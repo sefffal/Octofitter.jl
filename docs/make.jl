4b8547bffac73ba0f11ce6f1b8a4d17f14b59645
William Thompson <wthompson@uvic.ca>
2024-02-01 20:28:42 -0800

    Rearrange sidebar

--- docs/make.jl
+++ docs/make.jl
@@ -14,31 +14,36 @@ makedocs(
     sitename="Octofitter.jl",
     pages = [
         "Home" => "index.md",
-        "Getting Started" => "getting-started.md",
+        "Installation" => "getting-started.md",
         "Tutorials" => [
             "Fit Relative Astrometry" => "rel-astrom.md",
             "Fit with Observable Priors" => "rel-astrom-obs.md",
             "Fit Radial Velocity" => "rv-1.md",
             "Fit Proper Motion Anomaly" => "pma.md",
-            "Fit Images" => "images.md",
+            "Fit Orbits to Images" => "images.md",
             "Fit RV and Proper Motion Anomaly" => "rv.md",
             "Fit RV and Relative Astrometry" => "fit-rv-astrom.md",
             "Fit Resonant Co-Planar Model" => "fit-coplanar.md",
             "Fit Interferometer Data" => "fit-interfere.md",
+        ],
+        "Statistical Methods" => [
+            # "Prior Predictive Checks" => "prior-pred.md",
+            # "Posterior Predictive Checks" => "post-pred.md",
+            "Simulation Based Calibration" => "sbc.md",
+            # "Leave-One-Out Cross Validation" => "psis-loo.md",
+        ],
+        "Documentation" => [
+            "Using Python" => "python.md",
+            "Priors" => "priors.md",
+            "Derived Variables" => "derived.md",
             "Loading and Saving Data" => "loading-saving.md",
             "Connecting Mass with Photometry" => "mass-photometry.md",
             "Custom Likelihoods" => "custom-likelihood.md",
-            "Python Guide" => "python.md",
-            "Simulation Based Calibration" => "sbc.md",
-        ],
-        "Documentation" => [
-            "Priors" => "priors.md",
-            "Derived Variables" => "derived.md",
             "Sampler" => "samplers.md",
             "Parallel Sampling" => "parallel-sampling.md",
             "Chains" => "chains.md",
             "Kepler Solver" => "kepler.md",
-            "API" => "api.md"
+            "Full API Documentation" => "api.md"
         ]
     ],
     format = Documenter.HTML(
