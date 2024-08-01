7f774a095c9afc5206248a95e6bf881d8835b57a
William Thompson <wthompson@uvic.ca>
2024-07-31 20:51:00 -0700

    Bugfix: system number type detection

--- OctofitterRadialVelocity/src/rv-absolute.jl
+++ OctofitterRadialVelocity/src/rv-absolute.jl
@@ -58,7 +58,7 @@ function Octofitter.ln_like(
     planet_orbits::NTuple{N,<:AbstractOrbit},
     num_epochs::Val{L}=Val(length(rvlike.table))
 ) where {L,N}
-    T = _system_number_type(θ_system)
+    T = Octofitter._system_number_type(θ_system)
     ll = zero(T)
 
     # Each RV instrument index can have it's own barycentric RV offset and jitter.
