1f1e654dd30f89bd3bc09753e37375aa921f2b25
William Thompson <wthompson@uvic.ca>
2024-11-25 10:10:01 -0800

    Use global BB optimization, then pathfinder for initialization

--- src/initialization.jl
+++ src/initialization.jl
@@ -60,7 +60,6 @@ function guess_starting_position(rng::Random.AbstractRNG, model::LogDensityModel
             if logpost_ofti > logpost
                 logpost = logpost_ofti
                 params = params_ofti
-                    # println("accepted OFTI")
             end
                 # println("logpost_after = ", logpost)
             if logpost > bestlogpost
@@ -208,6 +207,8 @@ function get_starting_point!!(model::LogDensityModel; kwargs...)
     return get_starting_point!!(Random.default_rng(), model; kwargs...)
 end
 
+using OptimizationOptimJL, OptimizationBBO
+
 """
     default_initializer!(model::LogDensityModel; initial_samples=100_000)
 
@@ -219,36 +220,48 @@ If this fails repeatedly, simply draw `initial_samples` from the prior and keepi
 function default_initializer!(model::LogDensityModel; kwargs...)
     return default_initializer!(Random.default_rng(), model; kwargs...)
 end
-function default_initializer!(rng::Random.AbstractRNG, model::LogDensityModel; initial_point = nothing, nruns=8, ntries=2, ndraws=1000, initial_samples=10000, verbosity=1)
-
-    local result_pf = nothing
-    local metric = nothing
+function default_initializer!(rng::Random.AbstractRNG, model::LogDensityModel; nruns=8, ntries=2, ndraws=1000, verbosity=1)
     ldm_any = LogDensityModelAny(model)
-    verbosity >= 1 && @info "Determining initial positions and metric using pathfinder"
+
+    # Pathfinder (and especially multipathfinder) do not work well with global optimization methods.
+    # Instead, we do a two-step process. 
+    # Find the global MAP point, then initialize multi-pathfinder in Gaussian ball around that point.
+
+    priors = Octofitter._list_priors(model.system)
+    lb = model.link(quantile.(priors,0.001))
+    ub = model.link(quantile.(priors,0.999))
+    f = OptimizationFunction(
+        (u,p)->-p.ℓπcallback(u),
+        grad=(G,u,p)->(G .= p.∇ℓπcallback(u)[2])
+    )
+    prob = Optimization.OptimizationProblem(f, model.link(quantile.(priors,0.5)), model; lb, ub)
+    Random.seed!(rand(rng, UInt64))
+    sol = solve(prob, BBO_adaptive_de_rand_1_bin(), rel_tol=1e-3, maxiters = 100_000, )
+    
+    model.starting_points = fill(sol.u, 1000)
+    initial_logpost_range = (-sol.objective, -sol.objective)
+    if verbosity > 1
+        @info "Found the global maximum logpost" MAP=-sol.objective
+    end
+
+    # TODO: we don't really need to use pathfinder in this case, we should look into
+    # more rigourous variational methods
+    local result_pf = nothing
+    verbosity >= 1 && @info "Determining initial positions using pathfinder, around that location."
     # It can sometimes hit a PosDefException sometimes when factoring a matrix.
     # When that happens, the next try usually succeeds.
     try
         for i in 1:ntries
             verbosity >= 3 && @info "Starting multipathfinder run"
-            init_sampler = function(rng, x) 
-                if isnothing(initial_point) || length(initial_point) < model.D
-                    if verbosity > 3
-                        @info "drawing new starting guess by sampling IID from priors"
-                    end
-                    initial_θ, mapv = guess_starting_position(rng,model,initial_samples)
-                    if verbosity > 3
-                        @info "Starting point drawn" initial_logpost=mapv
+            init_sampler = function(rng, x)
+                for _ in 1:10
+                    # take a random step away from the MAP value according to the gradient
+                    x .= sol.u -0.1 .* rand.(rng) .* model.∇ℓπcallback(sol.u)[2] 
+                    if all(lb .< x .< ub) && isfinite(model.ℓπcallback(x))
+                        return
                     end
                 end
-                if !isnothing(initial_point)
-                    if length(initial_point) < model.D
-                        initial_θ = (initial_point..., initial_θ[length(initial_point)+1:end]...)
-                    else
-                        initial_θ = initial_point
-                    end
-                end
-                initial_θ_t = model.link(initial_θ)
-                x .= initial_θ_t
+                error("Could not find starting point within 0.001 - 0.999 quantiles of priors.")
             end
             errlogger = ConsoleLogger(stderr, verbosity >=3 ? Logging.Info : Logging.Error)
             initial_mt = _kepsolve_use_threads[]
@@ -257,7 +270,7 @@ function default_initializer!(rng::Random.AbstractRNG, model::LogDensityModel; i
                 result_pf = Pathfinder.multipathfinder(
                     ldm_any, ndraws;
                     nruns,
-                    init_sampler=CallableAny(init_sampler),
+                    init_sampler,
                     progress=verbosity > 1,
                     maxiters=25_000,
                     reltol=1e-6,
@@ -285,40 +298,36 @@ function default_initializer!(rng::Random.AbstractRNG, model::LogDensityModel; i
             verbosity > 2 &&  display(result_pf)
             break
         end
-    catch
-    end
-    
-    if !isnothing(result_pf)
-        model.starting_points = collect.(eachcol(result_pf.draws))
-        logposts = model.ℓπcallback.(model.starting_points)
-        initial_logpost_range = extrema(logposts)
-    end
-    # Occasionally there is a failure mode of pathfinder where, despite starting it at a reasonable spot, it returns garbage
-    # starting draws that are orders of magnitude worse.
-    # Check for this by ensuring the highest a-posteriori pathfinder draw is better than a random guess
-    _, random_guess_logpost = guess_starting_position(rng,model,100)
-    if isnothing(result_pf) || maximum(initial_logpost_range) < random_guess_logpost
-        if !isnothing(result_pf) 
-            verbosity >= 1 && @warn "The highest posterior density sample from pathfinder is worse than a random guess..."
-        end
-        verbosity >= 1 && @warn "Falling back to sampling from the prior and keeping the $ndraws samples with highest posterior density."
-        samples_t = map(1:1000) do _
-            initial_θ, mapv = guess_starting_position(rng,model,max(1,initial_samples÷100))
-            initial_θ_t = model.link(initial_θ)
-            return initial_θ_t
-        end
-        # samples = sample_priors(rng, model, ndraws)
-        # samples_t = model.link.(samples)
-        logposts = model.ℓπcallback.(samples_t)
-        II = sortperm(logposts, rev=true)[begin:ndraws]
-        model.starting_points = samples_t[II]
-        initial_logpost_range = extrema(@view logposts[II])
-        logposts = logposts[II]
+    catch err
+        @warn err
     end
 
-    if verbosity >= 1
-        @info "Found a sample of initial positions" initial_logpost_range
+    if !isnothing(result_pf)
+        model.starting_points = collect.(eachcol(result_pf.draws))
+    end
+    logposts = model.ℓπcallback.(model.starting_points)
+    initial_logpost_range = extrema(logposts)
+
+    if initial_logpost_range[2] < -sol.objective - 10 
+        if verbosity >= 1
+            @warn "Pathfinder produced samples with log-likelihood 10 worse than global max. Will just initialize at global max."
+        end
+        model.starting_points = fill(sol.u, 1000)
+        logposts = fill(-sol.objective, 1000)
+        initial_logpost_range = (-sol.objective, -sol.objective)
+    else
+        if verbosity >= 1
+            @info "Found a sample of initial positions" initial_logpost_range
+        end
     end
 
     return model.arr2nt(model.invlink(model.starting_points[argmax(logposts)]))
+end
+
+
+# Helper function for testing that the pathfinder initialization gives reasonable results
+function _startingpoints2chain(model)
+    solnts =  [(;logpost=0,model.arr2nt(model.invlink(s))...,) for s in model.starting_points]
+    chn = Octofitter.result2mcmcchain(solnts,  Dict(:internals => [:logpost]))
+    return chn
 end
