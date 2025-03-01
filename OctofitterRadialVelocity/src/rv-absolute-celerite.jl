722b758be6d7d533b6ece4501245d2fd55e8dc10
William Thompson <wthompson@uvic.ca>
2025-02-28 16:51:56 -0800

    Add support for cross-validation with Celerite GPs

--- OctofitterRadialVelocity/src/rv-absolute-celerite.jl
+++ OctofitterRadialVelocity/src/rv-absolute-celerite.jl
@@ -26,53 +26,65 @@ In addition to the example above, any Tables.jl compatible source can be provide
 """
 struct StarAbsoluteRVLikelihood_Celerite{TTable<:Table,GP,TF,offset_symbol,jitter_symbol} <: Octofitter.AbstractLikelihood
     table::TTable
+    held_out_table::TTable
     instrument_name::String
     gaussian_process::GP
     trend_function::TF
     offset_symbol::Symbol
     jitter_symbol::Symbol
-    function StarAbsoluteRVLikelihood_Celerite(
-        observations...;
-        offset,
-        jitter,
-        trend_function=(θ_system, epoch)->zero(Octofitter._system_number_type(θ_system)),
-        instrument_name="",
-        gaussian_process=nothing
-    )
-        table = Table(observations...)
-        if !Octofitter.equal_length_cols(table)
-            error("The columns in the input data do not all have the same length")
-        end
-        if !issubset(rv_cols, Tables.columnnames(table))
-            error("Expected columns $rv_cols")
-        end
-        if hasproperty(table, :inst_idx) && length(unique(table.inst_idx)) > 1
-            error("Deprecated: data from separate RV instruments should now be placed into different StarAbsoluteRVLikelihood_Celerite likelihood objects, rather than specified by an inst_idx parameter.")
-        end
-        rows = map(eachrow(table)) do row′
-            row = (;row′[1]..., rv=float(row′[1].rv[1]))
-            return row
-        end
-        # We sort the data first by instrument index then by epoch to make some later code faster
-        m = maximum(r->r.epoch, rows)
-        ii = sortperm([r.epoch for r in rows])
-        table = Table(rows[ii])
-        if isnothing(instrument_name)
-            instrument_name = string.(1:maximum(table.inst_idx))
-        end
-
-        return new{typeof(table),typeof(gaussian_process),typeof(trend_function),offset, jitter, }(table, instrument_name, gaussian_process, trend_function, offset, jitter, )
+end
+function StarAbsoluteRVLikelihood_Celerite(
+    observations...;
+    offset,
+    jitter,
+    trend_function=(θ_system, epoch)->zero(Octofitter._system_number_type(θ_system)),
+    instrument_name="",
+    gaussian_process=nothing
+)
+    table = Table(observations...)[:,:,1]
+    if !Octofitter.equal_length_cols(table)
+        error("The columns in the input data do not all have the same length")
     end
+    if !issubset(rv_cols, Tables.columnnames(table))
+        error("Expected columns $rv_cols")
+    end
+    if hasproperty(table, :inst_idx) && length(unique(table.inst_idx)) > 1
+        error("Deprecated: data from separate RV instruments should now be placed into different StarAbsoluteRVLikelihood_Celerite likelihood objects, rather than specified by an inst_idx parameter.")
+    end
+    rows = map(eachrow(table)) do row′
+        row = (;row′[1]..., rv=float(row′[1].rv[1]))
+        return row
+    end
+    # We sort the data first by instrument index then by epoch to make some later code faster
+    m = maximum(r->r.epoch, rows)
+    ii = sortperm([r.epoch for r in rows])
+    table = Table(rows[ii])
+    if isnothing(instrument_name)
+        instrument_name = string.(1:maximum(table.inst_idx))
+    end
+
+    # We need special book keeping for computing cross-validataion scores
+    # We keep a table of "held out" data if needed for that purpose.
+    # Here we leave it empty.
+    held_out_table = empty(table)
+
+    return new{typeof(table),typeof(gaussian_process),typeof(trend_function),offset, jitter, }(
+        table, held_out_table, instrument_name, gaussian_process, trend_function, offset, jitter
+    )
 end
 StarAbsoluteRVLikelihood_Celerite(observations::NamedTuple...;kwargs...) = StarAbsoluteRVLikelihood_Celerite(observations; kwargs...)
 function Octofitter.likeobj_from_epoch_subset(obs::StarAbsoluteRVLikelihood_Celerite, obs_inds)
-    return StarAbsoluteRVLikelihood_Celerite(
-        obs.table[obs_inds,:,1]...;
-        offset=obs.offset_symbol,
-        jitter=obs.jitter_symbol,
-        trend_function=obs.trend_function,
-        instrument_name=obs.instrument_name,
-        gaussian_process=obs.gaussian_process,
+    # Due to TypedTables bug, the line below creates a "matrix" table that isn't the same type as the input.
+    # table = typeof(obs.table)(obs.table[setdiff(1:size(obs.table,1), obs_inds),:,1])
+    # table = Table(collect(eachrow(obs.table))[setdiff(1:size(obs.table,1), obs_inds)]...)
+    table = Table(first(eachcol(obs.table[setdiff(1:size(obs.table,1), obs_inds)])))
+    if obs_inds isa Number
+        held_out_table = obs.table[obs_inds,:,1]
+    else
+        held_out_table = Table(first(eachcol(obs.table[obs_inds])))
+    end
+    return StarAbsoluteRVLikelihood_Celerite{typeof(table),typeof(obs.gaussian_process),typeof(obs.trend_function),obs.offset_symbol, obs.jitter_symbol, }(
+        table, held_out_table, obs.instrument_name, obs.gaussian_process, obs.trend_function, obs.offset_symbol, obs.jitter_symbol
     )
 end
 export StarAbsoluteRVLikelihood_Celerite
@@ -103,19 +115,11 @@ function Octofitter.ln_like(
 
     @no_escape begin
 
-        # Each RV instrument index can have it's own barycentric RV offset and jitter.
-        # Grab the offsets/jitters and store in a tuple. 
-        # Then we can index by inst_idxs to look up the right offset and jitter.
 
         # Vector of radial velocity of the star at each epoch. Go through and sum up the influence of
         # each planet and put it into here. 
-        rv_buf =  @alloc(T, L) # @SArray
-        rv_var_buf =   @alloc(T, L) # @SArray
-
-        # Data for this instrument:
-        # epochs = rvlike.table.epoch
-        # σ_rvs = rvlike.table.σ_rv
-        # rvs = rvlike.table.rv
+        rv_buf =  @alloc(T, L)
+        rv_var_buf =   @alloc(T, L)
 
         # RV "data" calculation: measured RV + our barycentric rv calculation
         rv_buf .= rvlike.table.rv .- offset .- rvlike.trend_function(θ_system, rvlike.table.epoch)
@@ -132,8 +136,6 @@ function Octofitter.ln_like(
                     planet_mass*Octofitter.mjup2msol
                 )
             end
-            # Zygote version:
-            # rv_buf -= radvel.(orbit, epochs, planet_mass*Octofitter.mjup2msol)
         end        
 
         # The noise variance per observation is the measurement noise and the jitter added
@@ -160,22 +162,60 @@ function Octofitter.ln_like(
                 end
             end
 
-            #     if err isa PosDefException
-            #         @warn "err" exception=(err, catch_backtrace()) maxlog=1
-            #         ll = convert(T,-Inf)
-            #     elseif err isa ArgumentError
-            #         @warn "err" exception=(err, catch_backtrace()) maxlog=1
-            #         ll = convert(T,-Inf)
-            #     else
-            #         rethrow(err)
-            #     end
-            # end             
-            
             if isfinite(ll)
-                # fx = gp(rvlike.table.epoch, rv_var_buf)
                 try
-                    # ll += logpdf(fx, rv_buf)
-                    ll += Celerite.log_likelihood(gp, rv_buf)
+
+                    if isempty(rvlike.held_out_table)
+                        ll += Celerite.log_likelihood(gp, rv_buf)
+                    else
+
+                        # If we have held out data, that means we are doing cross-validataion ----
+                        # we are conditioning on a subset of data, and computing the likelihood for
+                        # the held out data. 
+
+
+                        # Vector of radial velocity of the star at each epoch. Go through and sum up the influence of
+                        # each planet and put it into here. 
+                        rv_buf_held_out =  @alloc(T, length(rvlike.held_out_table.epoch))
+                        rv_var_buf_held_out =  @alloc(T, length(rvlike.held_out_table.epoch))
+
+                        # RV "data" calculation: measured RV + our barycentric rv calculation
+                        rv_buf_held_out .= rvlike.held_out_table.rv .- offset .- rvlike.trend_function(θ_system, rvlike.held_out_table.epoch)
+
+                        # Go through all planets and subtract their modelled influence on the RV signal:
+                        # You could consider `rv_star` as the residuals after subtracting these.
+                        
+                        for planet_i in eachindex(planet_orbits)
+                            orbit = planet_orbits[planet_i]
+                            planet_mass = θ_system.planets[planet_i].mass
+                            for epoch_i in eachindex(rvlike.held_out_table.epoch)
+                                rv_buf_held_out[epoch_i] -= radvel(
+                                    # We can't look into the pre-populated orbit solutions here, since these 
+                                    # are only generated for entries in an likelihood objects `table`.
+                                    # We have to solve it ourselves as we go. This should have negligible
+                                    # performance impact unless we are holding out many data points and
+                                    # we would miss the multi-threaded solve.
+                                    # orbit_solutions[planet_i][epoch_i+orbit_solutions_i_epoch_start],
+                                    orbitsolve(orbit, rvlike.held_out_table.epoch[epoch_i]),
+                                    planet_mass*Octofitter.mjup2msol
+                                )
+                            end
+                        end        
+
+                        # The noise variance per observation is the measurement noise and the jitter added
+                        # in quadrature
+                        rv_var_buf_held_out .= rvlike.held_out_table.σ_rv.^2 .+ jitter^2
+                        
+                        # Compute GP model
+                        # Main.Celerite.compute!(...) # we've already conditioned the model on the
+                        # non-held-out data above
+
+                        pred, var = Main.Celerite.predict(gp, rv_buf, rvlike.held_out_table.epoch; return_var=true)
+                        for i_held_out in 1:size(rvlike.held_out_table.epoch,1)
+                            ll += logpdf(Normal(pred[i_held_out], sqrt(var[i_held_out] + rv_var_buf_held_out[i_held_out])), rv_buf_held_out[i_held_out])
+                        end
+                    end
+
                 catch err
                     if err isa PosDefException || err isa DomainError
                         @warn "err" exception=(err, catch_backtrace()) θ_system
--- src/cross-validation.jl
+++ src/cross-validation.jl
@@ -123,85 +123,245 @@ function generate_kfold_systems(system)
     return per_obs_systems
 end
 
+
 """
-Given an existing model with N epochs of data spread across any number of likelihood objects, return N copies
-that only contain data from a given epoch.
+Given an existing model with N likelihood objects, run a user-provided callback
+to decide which likelihood objects to retain
 """
-function generate_system_per_epoch(system)
-    
+function generate_system_filtered_like(user_predicate, system)
     # We will number observations by index in their table (if there is a table)
     # (if no table, just discard observation object). Start with observations of star,
     # then observations attached to each planet.
-    observation_count = _count_epochs(system)
+    # Yikes!
+    likeobj_count = _count_likeobj(system)
     
-    per_obs_systems = map(1:observation_count) do i_obs
-        j_obs = 0
-        for obs in system.observations
-            if hasproperty(obs, :table)
-                for k_obs in 1:Tables.rowcount(obs.table)
-                    j_obs += 1
-                    if i_obs == j_obs
-                        obs_row = likeobj_from_epoch_subset(obs, k_obs)
-                        return System(
-                            system.priors,
-                            system.derived,
-                            obs_row;
-                            system.name
-                        )
-                    end
+    j_obs = 0
+    to_include_system = []
+    to_include_planets = []
+    included_j_obs = Int[]
+    for obs in system.observations
+        if _isprior(obs)
+            push!(to_include_system, obs)
+        else
+            j_obs += 1
+            if user_predicate(obs)
+                push!(to_include_system, likeobj_from_epoch_subset(obs, :))
+                push!(included_j_obs, j_obs)
+            end
+        end
+        # TODO, if prior always include
+    end
+    for planet in system.planets
+        to_include_planet = []
+        push!(to_include_planets, to_include_planet)
+        for obs in planet.observations
+            if _isprior(obs)
+                push!(to_include_planet, obs)
+            else
+                j_obs += 1
+                if user_predicate(obs)
+                    obs_row = likeobj_from_epoch_subset(obs, :)
+                    push!(to_include_planet, likeobj_from_epoch_subset(obs, :))
+                    push!(included_j_obs, j_obs)
                 end
+            end
+        end
+    end
+
+    planets_new = map(system.planets, to_include_planets) do planet, like_objs
+        planet = Planet{orbittype(planet)}(
+            planet.priors,
+            planet.derived,
+            like_objs...;
+            planet.name
+        )
+    end
+
+    new_name = Symbol(string(system.name, "_filt_obs_", join(included_j_obs, '_')))
+    return System(
+        system.priors,
+        system.derived,
+        to_include_system...,
+        planets_new...;
+        name=new_name
+    )
+
+end
+
+"""
+Given an existing model with N likelihood objects, return N copies
+that each have only one likelihood attached
+"""
+function generate_systems_per_like(system)
+    # We will number observations by index in their table (if there is a table)
+    # (if no table, just discard observation object). Start with observations of star,
+    # then observations attached to each planet.
+    # Yikes!
+    likeobj_count = _count_likeobj(system)
+    
+    per_obs_systems = map(1:likeobj_count) do i_obs
+        j_obs = 0
+        to_include_system = []
+        to_include_planets = []
+        for obs in system.observations
+            if _isprior(obs)
+                push!(to_include_system, obs)
             else
                 j_obs += 1
                 if i_obs == j_obs
-                    return System(
-                        system.priors,
-                        system.derived,
-                        obs;
-                        system.name
-                    )
+                    push!(to_include_system, likeobj_from_epoch_subset(obs, :))
                 end
             end
+            # TODO, if prior always include
         end
         for planet in system.planets
+            to_include_planet = []
+            push!(to_include_planets, to_include_planet)
             for obs in planet.observations
-                if hasproperty(obs, :table)
-                    for k_obs in 1:Tables.rowcount(obs.table)
-                        j_obs += 1
-                        if i_obs == j_obs
-                            obs_row = likeobj_from_epoch_subset(obs, k_obs)
-                            planet = Planet{orbittype(planet)}(
-                                planet.priors,
-                                planet.derived,
-                                (obs_row,);
-                                planet.name
-                            )
-                            return System(
-                                system.priors,
-                                system.derived,
-                                planet;
-                                system.name
-                            )
-                        end
-                    end
+                if _isprior(obs)
+                    push!(to_include_planet, obs)
                 else
                     j_obs += 1
                     if i_obs == j_obs
-                        planet = Planet{orbittype(planet)}(
-                            planet.priors,
-                            planet.derived,
-                            (obs,);
-                            planet.name
-                        )
-                        return System(
-                            system.priors,
-                            system.derived,
-                            planet;
-                            system.name
-                        )
+                        obs_row = likeobj_from_epoch_subset(obs, :)
+                        push!(to_include_planet, likeobj_from_epoch_subset(obs, :))
                     end
                 end
             end
         end
+
+        planets_new = map(system.planets, to_include_planets) do planet, like_objs
+            planet = Planet{orbittype(planet)}(
+                planet.priors,
+                planet.derived,
+                like_objs...;
+                planet.name
+            )
+        end
+
+        new_name = Symbol(string(system.name, "_like_$i_obs"))
+        return System(
+            system.priors,
+            system.derived,
+            to_include_system...,
+            planets_new...;
+            name=new_name
+        )
+    end
+
+    return per_obs_systems
+end
+
+"""
+Given an existing model with N epochs of data spread across any number of likelihood objects, return N copies
+that only contain data from a given epoch. Non-tabular/_isprior observations are included in all systems.
+"""
+function generate_system_per_epoch(system)
+    
+    # First, collect all tabular observations and count how many total epochs we have
+    tabular_observations = []
+    
+    # Collect system-level tabular observations
+    for obs in system.observations
+        if hasproperty(obs, :table)
+            push!(tabular_observations, (obs=obs, planet_idx=nothing, row_count=Tables.rowcount(obs.table)))
+        end
+    end
+    
+    # Collect planet-level tabular observations
+    for (planet_idx, planet) in enumerate(system.planets)
+        for obs in planet.observations
+            if hasproperty(obs, :table)
+                push!(tabular_observations, (obs=obs, planet_idx=planet_idx, row_count=Tables.rowcount(obs.table)))
+            end
+        end
+    end
+    
+    # Calculate total epoch count from tabular observations
+    total_epochs = sum(item -> item.row_count, tabular_observations)
+    
+    # If there are no epochs, return the original system
+    if total_epochs == 0
+        return [system]
+    end
+    
+    # Create a system for each epoch
+    per_obs_systems = map(1:total_epochs) do i_obs
+        # Initialize collections for observations
+        to_include_system = []
+        
+        # Always include non-tabular system observations in every system
+        for obs in system.observations
+            if !hasproperty(obs, :table)
+                push!(to_include_system, obs)
+            end
+        end
+        
+        # Track current epoch index
+        current_epoch = 0
+        
+        # Add the matching tabular observation for this epoch
+        for tab_obs in tabular_observations
+            # Skip planet observations for now
+            if tab_obs.planet_idx !== nothing
+                continue
+            end
+            
+            # For each row in the table
+            for k_row in 1:tab_obs.row_count
+                current_epoch += 1
+                if current_epoch == i_obs
+                    # This is the matching epoch, add just this row
+                    push!(to_include_system, likeobj_from_epoch_subset(tab_obs.obs, k_row))
+                    break
+                end
+            end
+        end
+        
+        # Process all planets
+        planets_new = map(1:length(system.planets)) do p_idx
+            planet = system.planets[p_idx]
+            to_include_planet = []
+            
+            # Always include non-tabular observations for this planet
+            for obs in planet.observations
+                if !hasproperty(obs, :table)
+                    push!(to_include_planet, obs)
+                end
+            end
+            
+            # Add matching tabular observation for this planet if it exists
+            for tab_obs in tabular_observations
+                if tab_obs.planet_idx == p_idx
+                    for k_row in 1:tab_obs.row_count
+                        current_epoch += 1
+                        if current_epoch == i_obs
+                            # This is the matching epoch, add just this row
+                            push!(to_include_planet, likeobj_from_epoch_subset(tab_obs.obs, k_row))
+                            break
+                        end
+                    end
+                end
+            end
+            
+            # Create new planet with included observations
+            return Planet{orbittype(planet)}(
+                planet.priors,
+                planet.derived,
+                to_include_planet...;
+                name=planet.name
+            )
+        end
+        
+        # Create the new system with all planets
+        new_name = Symbol(string(system.name, "_epoch_$i_obs"))
+        return System(
+            system.priors,
+            system.derived,
+            to_include_system...,
+            planets_new...;
+            name=new_name
+        )
     end
 
     return per_obs_systems
