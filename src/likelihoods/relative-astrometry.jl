08948c021cc5a66fe5c2a03783572ae1e98f6add
William Thompson <wthompson@uvic.ca>
2025-03-17 19:34:37 -0700

    Add support for jitters and offsets in groups of relative astrometry data

--- src/likelihoods/relative-astrometry.jl
+++ src/likelihoods/relative-astrometry.jl
@@ -16,10 +16,18 @@ All units are in **milliarcseconds** or **radians** as appropriate.
 
 In addition to the example above, any Tables.jl compatible source can be provided.
 """
-struct PlanetRelAstromLikelihood{TTable<:Table,TDistTuple} <: AbstractLikelihood
+struct PlanetRelAstromLikelihood{TTable<:Table,TDistTuple,jitter,platescale,northangle} <: AbstractLikelihood
     table::TTable
+    jitter::Symbol
+    platescale::Symbol
+    northangle::Symbol
     precomputed_pointwise_distributions::TDistTuple
-    function PlanetRelAstromLikelihood(observations...)
+    function PlanetRelAstromLikelihood(
+            observations...;
+            jitter::Symbol=:__NA,
+            platescale::Symbol=:__NA,
+            northangle::Symbol=:__NA,
+        )
         table = Table(observations...)
         if !equal_length_cols(table)
             error("The columns in the input data do not all have the same length")
@@ -64,11 +72,36 @@ struct PlanetRelAstromLikelihood{TTable<:Table,TDistTuple} <: AbstractLikelihood
         end
 
         precomputed_pointwise_distributions_tuple = (precomputed_pointwise_distributions...,)
-        return new{typeof(table),typeof(precomputed_pointwise_distributions_tuple)}(table, precomputed_pointwise_distributions_tuple)
+        return new{typeof(table),typeof(precomputed_pointwise_distributions_tuple),jitter, platescale, northangle}(
+            table, jitter, platescale, northangle, precomputed_pointwise_distributions_tuple
+        )
     end
 end
 PlanetRelAstromLikelihood(observations::NamedTuple...) = PlanetRelAstromLikelihood(observations)
 export PlanetRelAstromLikelihood
+
+function _getparams(::PlanetRelAstromLikelihood{TTable,TDistTuple,jitter,platescale,northangle}, θ) where {TTable,TDistTuple,jitter,platescale,northangle}
+
+    jitter,platescale,northangle
+
+    jitter_val = zero(Octofitter._system_number_type(θ))
+    if jitter != :__NA
+        jitter_val = getproperty(θ, jitter)
+    end
+    platescale_val = one(Octofitter._system_number_type(θ))
+    if platescale != :__NA
+        platescale_val = getproperty(θ, platescale)
+    end
+    northangle_val = zero(Octofitter._system_number_type(θ))
+    if northangle != :__NA
+        northangle_val = getproperty(θ, northangle)
+    end
+    return (;
+        jitter=jitter_val,
+        platescale=platescale_val,
+        northangle=northangle_val,
+    )
+end
 
 function likeobj_from_epoch_subset(obs::PlanetRelAstromLikelihood, obs_inds)
     return PlanetRelAstromLikelihood(obs.table[obs_inds,:,1]...)
@@ -82,6 +115,7 @@ function ln_like(astrom::PlanetRelAstromLikelihood, θ_system, θ_planet, orbits
 
     # Note: since astrometry data is stored in a typed-table, the column name
     # checks using `hasproperty` ought to be compiled out completely.
+    (;jitter, platescale, northangle,) = _getparams(astrom, merge(θ_system, θ_planet))
 
     this_orbit = orbits[i_planet]
     T = _system_number_type(θ_system)
@@ -94,15 +128,17 @@ function ln_like(astrom::PlanetRelAstromLikelihood, θ_system, θ_planet, orbits
         # due to inner orbits.
         # Note! this is essentially free to calculate, since we already pre-solved
         # all orbits at all epochs.
-
+        sol = orbit_solutions[i_planet][i_epoch + orbit_solutions_i_epoch_start]
+        # println(astrom.table.epoch[i_epoch], "\t", sol.sol.t)
+        @assert astrom.table.epoch[i_epoch] == PlanetOrbits.soltime(sol)
         ra_host_perturbation = zero(T)
         dec_host_perturbation = zero(T)
-        for i_other_planet in eachindex(orbits)
+        for (i_other_planet, key) in enumerate(keys(θ_system.planets))
             orbit_other = orbits[i_other_planet]
             # Only account for inner planets
             # if semimajoraxis(orbit_other) == semimajoraxis(this_orbit)
             if semimajoraxis(orbit_other) < semimajoraxis(this_orbit)
-                θ_planet′ = θ_system.planets[i_other_planet]
+                θ_planet′ = θ_system.planets[key]
                 mass_other = θ_planet′.mass*Octofitter.mjup2msol
                 sol′ = orbit_solutions[i_other_planet][i_epoch + orbit_solutions_i_epoch_start]
                 # Note about `total mass`: for this to be correct, user will have to specify
@@ -113,6 +149,9 @@ function ln_like(astrom::PlanetRelAstromLikelihood, θ_system, θ_planet, orbits
 
                 ra_host_perturbation += raoff(sol′, mass_other)
                 dec_host_perturbation += decoff(sol′, mass_other)
+
+                @assert astrom.table.epoch[i_epoch] == PlanetOrbits.soltime(sol)
+                @assert astrom.table.epoch[i_epoch] == PlanetOrbits.soltime(sol′)
             end
         end
 
@@ -131,18 +170,42 @@ function ln_like(astrom::PlanetRelAstromLikelihood, θ_system, θ_planet, orbits
             ρ = hypot(ra_model,dec_model)
             pa = atan(ra_model,dec_model)
 
-            pa_diff = ( astrom.table.pa[i_epoch] - pa + π) % 2π - π;
+            pa_dat = astrom.table.pa[i_epoch] + northangle
+            pa_diff = ( pa_dat - pa + π) % 2π - π;
             pa_diff = pa_diff < -π ? pa_diff + 2π : pa_diff;
             resid1 = pa_diff
-            resid2 = astrom.table.sep[i_epoch] - ρ
+            resid2 = astrom.table.sep[i_epoch]*platescale - ρ
 
         # RA and DEC specified
         else
-            resid1 = astrom.table.ra[i_epoch]  - ra_model
-            resid2 = astrom.table.dec[i_epoch]  -  dec_model
+            pa_dat = atan(astrom.table.dec[i_epoch], astrom.table.ra[i_epoch]) + northangle
+            sep_dat = hypot(astrom.table.dec[i_epoch], astrom.table.ra[i_epoch])*platescale
+            ra_dat = sep_dat * cos(pa_dat)
+            dec_dat = sep_dat * sin(pa_dat)
+            resid1 = ra_dat - ra_model
+            resid2 = dec_dat -  dec_model
         end
 
-        ll += logpdf(astrom.precomputed_pointwise_distributions[i_epoch], @SVector[resid1, resid2])
+        if jitter == 0.
+            ll += logpdf(astrom.precomputed_pointwise_distributions[i_epoch], @SVector[resid1, resid2])
+        else
+            # Add jitter in quadrature
+            σ₁ = hypot(σ₁, jitter)
+            σ₂ = hypot(σ₂, jitter)
+            # we have to compute the factorization on the fly
+            if hasproperty(astrom.table, :cor)
+                cor = table.cor[i]
+                Σ = @SArray[
+                    σ₁^2        cor*σ₁*σ₂
+                    cor*σ₁*σ₂   σ₂^2
+                ]
+                dist = MvNormal(Σ)
+            else
+                Σ = Diagonal(@SArray[σ₁^2, σ₂^2])
+                dist = MvNormal(Σ)
+            end
+            ll += logpdf(dist, @SVector[resid1, resid2])
+        end
     end
     return ll
 end
