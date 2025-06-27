4ab18553ed95745d1bbd2e00044f69bfb6e0ebca
William Thompson <wthompson@uvic.ca>
2025-06-27 14:27:47 -0700

    Support serializing result named tuples to chains even if they contain arbitrary objects (just skip these)

--- src/sampling.jl
+++ src/sampling.jl
@@ -430,7 +430,7 @@ function result2mcmcchain(chain_in, sectionmap=Dict())
             end
             if sample[key] isa Number
                 push!(vals, sample[key])
-            else
+            elseif sample[key] isa AbstractArray || sample[key] isa Tuple
                 for val in sample[key]
                     push!(vals, val)
                 end
@@ -442,7 +442,7 @@ function result2mcmcchain(chain_in, sectionmap=Dict())
             for key in keys(sample.observations[obs])
                 if sample.observations[obs][key] isa Number
                     push!(vals, sample.observations[obs][key])
-                else
+                elseif sample.observations[obs][key] isa AbstractArray || sample.observations[obs][key] isa Tuple
                     for val in sample.observations[obs][key]
                         push!(vals, val)
                     end
@@ -458,7 +458,7 @@ function result2mcmcchain(chain_in, sectionmap=Dict())
                 end
                 if sample.planets[pl][key] isa Number
                     push!(vals, sample.planets[pl][key])
-                else
+                elseif sample.planets[pl][key] isa AbstractArray || sample.planets[pl][key] isa Tuple
                     for val in sample.planets[pl][key]
                         push!(vals, val)
                     end
@@ -470,7 +470,7 @@ function result2mcmcchain(chain_in, sectionmap=Dict())
                 for key in keys(sample.planets[pl].observations[obs])
                     if sample.planets[pl].observations[obs][key] isa Number
                         push!(vals, sample.planets[pl].observations[obs][key])
-                    else
+                    elseif sample.planets[pl].observations[obs][key] isa AbstractArray || sample.planets[pl].observations[obs][key] isa Tuple
                         for val in sample.planets[pl].observations[obs][key]
                             push!(vals, val)
                         end
@@ -750,7 +750,7 @@ function flatten_named_tuple(nt)
         end
         if nt[key] isa Number
             push!(pairs, key => nt[key])
-        else
+        elseif nt[key] isa AbstractArray || nt[key] isa Tuple
             for i in eachindex(nt[key])
                 key_i = Symbol(key, '_', i)
                 push!(pairs, key_i => nt[key][i])
@@ -763,7 +763,7 @@ function flatten_named_tuple(nt)
         for key in keys(nt.observations[obs])
             if nt.observations[obs][key] isa Number
                 push!(pairs, Symbol(obs, '_', key) => nt.observations[obs][key])
-            else
+            elseif nt.observations[obs][key] isa AbstractArray || nt.observations[obs][key] isa Tuple
                 for i in eachindex(nt.observations[obs][key])
                     push!(pairs, Symbol(obs, '_', key, '_', i) => nt.observations[obs][key][i])
                 end
@@ -779,7 +779,7 @@ function flatten_named_tuple(nt)
             end
             if nt.planets[pl][key] isa Number
                 push!(pairs, Symbol(pl, '_', key) => nt.planets[pl][key])
-            else
+            elseif nt.planets[pl][key] isa AbstractArray || nt.planets[pl][key] isa Tuple
                 for i in eachindex(nt.planets[pl][key])
                     push!(pairs, Symbol(pl, '_', key, '_', i) => nt.planets[pl][key][i])
                 end
@@ -791,7 +791,7 @@ function flatten_named_tuple(nt)
             for key in keys(nt.planets[pl].observations[obs])
                 if nt.planets[pl].observations[obs][key] isa Number
                     push!(pairs, Symbol(pl, '_', obs, '_', key) => nt.planets[pl].observations[obs][key])
-                else
+                elseif nt.planets[pl].observations[obs][key] isa AbstractArray || nt.planets[pl].observations[obs][key] isa Tuple
                     for i in eachindex(nt.planets[pl].observations[obs][key])
                         push!(pairs, Symbol(pl, '_', obs, '_', key, '_', i) => nt.planets[pl].observations[obs][key][i])
                     end
