5fe21ab6af689a824be434675597cd42517c84fc
William Thompson <wthompson@uvic.ca>
2025-08-04 15:31:28 -0700

    Add observable prior for rv data

--- src/likelihoods/prior-observable.jl
+++ src/likelihoods/prior-observable.jl
@@ -66,7 +66,8 @@ struct ObsPriorAstromONeil2019{Likelihood<:AbstractLikelihood} <: AbstractLikeli
 end
 export ObsPriorAstromONeil2019
 likelihoodname(obs::ObsPriorAstromONeil2019) = "obspri_" * likelihoodname(obs.wrapped_like)
-_isprior(::ObsPriorAstromONeil2019) = true
+# since it wraps data, and only impacts the likelihood through calculations involving data points, we don't include it in the "prior" models
+_isprior(::ObsPriorAstromONeil2019) = false
 
 function likeobj_from_epoch_subset(obs::ObsPriorAstromONeil2019, obs_inds)
     return ObsPriorAstromONeil2019(
@@ -75,16 +76,17 @@ function likeobj_from_epoch_subset(obs::ObsPriorAstromONeil2019, obs_inds)
 end
 
 function ln_like(like::ObsPriorAstromONeil2019{<:PlanetRelAstromLikelihood}, θ_system, θ_planet, θ_obs, orbits, orbit_solutions, i_planet, orbit_solutions_i_epoch_start)
+    T = _system_number_type(θ_system)
 
     # Call the wrapped likelihood's ln_like method
     ln_like_wrapped = ln_like(like.wrapped_like, θ_system, θ_planet, θ_obs, orbits, orbit_solutions, i_planet, orbit_solutions_i_epoch_start)
 
     orbit = orbits[i_planet]
     # Add period prior
-    ln_prior = 0.0
+    ln_prior = zero(T)
     P = period(orbit)/365.25
     e = eccentricity(orbit)
-    jac = 0.0
+    jac = zero(T)
     # We can't use i_orbsol_start for this, since we're actually using the epochs
     # of another likelihood object. Instead loop through each until we find the right epoch.
     # This should normally be faster than solving it again.
@@ -126,6 +128,65 @@ function ln_like(like::ObsPriorAstromONeil2019{<:PlanetRelAstromLikelihood}, θ_
     jac *= cbrt(P) / sqrt_eccen 
 
     ln_prior += 2log(jac)
+
+    return ln_like_wrapped + ln_prior
+end
+
+function ln_like(like::ObsPriorAstromONeil2019{<:Any}, θ_system, θ_obs, orbits, orbit_solutions, orbit_solutions_i_epoch_start)
+
+    T = _system_number_type(θ_system)
+
+    # Call the wrapped likelihood's ln_like method
+    ln_like_wrapped = ln_like(like.wrapped_like, θ_system, θ_obs, orbits, orbit_solutions, orbit_solutions_i_epoch_start)
+    ln_prior = zero(T)
+
+    for (i_planet, orbit) in enumerate(orbits)
+        # Add period prior
+        P = period(orbit)/365.25
+        e = eccentricity(orbit)
+        jac = 0.0
+        # We can't use i_orbsol_start for this, since we're actually using the epochs
+        # of another likelihood object. Instead loop through each until we find the right epoch.
+        # This should normally be faster than solving it again.
+        # TODO: I wonder if a Dict would be a better choice here. Something to benchmark.
+        # Or maybe a dict of likelihood object to starting index?
+        # TODO: problem: this doesn't work with AbsoluteVisual orbits, since the true time is different
+        # due to changing light travel time.
+        for j in 1:length(like.wrapped_like.table.epoch)
+            current_epoch = like.wrapped_like.table.epoch[j]
+            local sol
+            found = false
+            for k in eachindex(orbit_solutions[i_planet])
+                sol′ = orbit_solutions[i_planet][k]
+                if hasproperty(sol′, :t) && sol′.t == current_epoch
+                    found = true
+                    sol = sol′
+                    break
+                elseif  hasproperty(sol′, :sol) && hasproperty(sol′.sol, :t) &&
+                        sol′.sol.t == current_epoch
+                    found = true
+                    sol = sol′
+                    break
+                end
+            end
+            # if !found
+            #     error("epoch not found in solutions $current_epoch")
+            # end
+            if !found
+                sol = orbitsolve(orbit, current_epoch)
+            end
+            M = meananom(sol)
+            E = eccanom(sol)
+            jac += abs(3M*(
+                e+cos(E)
+            ) + 2*(-2+e^2+e*cos(E)) *sin(E))
+        end
+
+        sqrt_eccen = sqrt(1-eccentricity(orbit)^2)
+        jac *= cbrt(P) / sqrt_eccen 
+
+        ln_prior += 2log(jac)
+    end
 
     return ln_like_wrapped + ln_prior
 end
